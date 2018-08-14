98f874397aff1c749703186b5b9caa3c046ddea8
Steve S. Mo <sdmo@naver.com>
2018-08-14 15:52:11 +0900

    changed TypingMachine destructor

---
+++ node_test.cc
@@ -0,0 +1,174 @@
+// Copyright 2018 Minkyu Jo
+
+#include "node.h"
+#include "node_test.h"
+#include "test.h"
+
+#include <stdlib.h>
+
+#define char_rand() '#'
+/*
+static char char_rand(void)
+{
+int rand_num = 0;
+do {
+rand_num = rand();
+} while (rand_num != 0);
+
+return (rand_num > 0 ? rand_num % 0xff : (-rand_num) % 0xff);
+}
+*/
+
+class NodeConstructorTestSuite
+{
+private:
+	static void SingleNodeTest() {
+		Node *node = new Node('A');
+		ASSERT_EQ(node->GetData(), 'A');
+		delete node;
+	}
+
+	static void GetDataTest() {
+		char temp = 0;
+		Node *node = new Node(temp);
+		ASSERT_EQ(node->GetData(), temp);
+		delete node;
+	}
+
+public:
+	static void RunTest() {
+		SingleNodeTest();
+		GetDataTest();
+	}
+};
+
+class NodeMethodTestSuite
+{
+private:
+	static void InsertPreviousNodeNullTest() { // also including GetPreviousNode() test
+		char temp1 = char_rand();
+		Node *node1 = new Node(temp1);
+
+		char temp2 = char_rand();
+		Node *node2 = node1->InsertPreviousNode(temp2);
+
+		ASSERT_EQ(node1->GetData(), temp1);
+		ASSERT_EQ(node2->GetData(), temp2);
+
+		ASSERT_EQ(node1->GetPreviousNode()->GetData(), temp2);
+
+		delete node1;
+		delete node2;
+	}
+
+	static void InsertPreviousNodeTest() { // also including GetPreviousNode() test
+		char temp1 = char_rand();
+		Node *node1 = new Node(temp1);
+
+		char temp3 = char_rand();
+		Node *node3 = node1->InsertPreviousNode(temp3);
+
+		char temp2 = char_rand();
+		Node *node2 = node1->InsertPreviousNode(temp2);
+
+		ASSERT_EQ(node1->GetData(), temp1);
+		ASSERT_EQ(node2->GetData(), temp2);
+		ASSERT_EQ(node3->GetData(), temp3);
+
+		ASSERT_EQ(node1->GetPreviousNode()->GetData(), temp2);
+		ASSERT_EQ(node1->GetPreviousNode()->GetPreviousNode()->GetData(), temp3);
+
+		delete node1;
+		delete node2;
+		delete node3;
+	}
+
+	static void InsertNextNodeTest() { // also including GetNextNode() test
+		char temp1 = char_rand();
+		Node *node1 = new Node(temp1);
+
+		char temp3 = char_rand();
+		Node *node3 = node1->InsertNextNode(temp3);
+
+		char temp2 = char_rand();
+		Node *node2 = node1->InsertNextNode(temp2);
+
+		ASSERT_EQ(node1->GetData(), temp1);
+		ASSERT_EQ(node2->GetData(), temp2);
+		ASSERT_EQ(node3->GetData(), temp3);
+
+		ASSERT_EQ(node1->GetNextNode()->GetData(), temp2);
+		ASSERT_EQ(node1->GetNextNode()->GetNextNode()->GetData(), temp3);
+
+		delete node1;
+		delete node2;
+		delete node3;
+	}
+
+	static void ErasePreviousNodeTest() {
+		char temp1 = char_rand();
+		Node *node1 = new Node(temp1);
+
+		char temp3 = char_rand();
+		Node *node3 = node1->InsertPreviousNode(temp3);
+
+		char temp2 = char_rand();
+		Node *node2 = node1->InsertPreviousNode(temp2);
+
+		ASSERT_EQ(node1->GetData(), temp1);
+		ASSERT_EQ(node2->GetData(), temp2);
+		ASSERT_EQ(node3->GetData(), temp3);
+
+		ASSERT_EQ(node1->GetPreviousNode()->GetData(), temp2);
+		node1->ErasePreviousNode();
+		ASSERT_EQ(node1->GetPreviousNode()->GetData(), temp3);
+
+		node1->ErasePreviousNode();
+		ASSERT_TRUE(node1->GetPreviousNode() == nullptr);
+
+		ASSERT_EQ(node1->ErasePreviousNode(), false);
+
+		delete node1;
+	}
+
+	static void EraseNextNodeTest() {
+		char temp1 = char_rand();
+		Node *node1 = new Node(temp1);
+
+		char temp3 = char_rand();
+		Node *node3 = node1->InsertNextNode(temp3);
+
+		char temp2 = char_rand();
+		Node *node2 = node1->InsertNextNode(temp2);
+
+		ASSERT_EQ(node1->GetData(), temp1);
+		ASSERT_EQ(node2->GetData(), temp2);
+		ASSERT_EQ(node3->GetData(), temp3);
+
+		ASSERT_EQ(node1->GetNextNode()->GetData(), temp2);
+		node1->EraseNextNode();
+		ASSERT_EQ(node1->GetNextNode()->GetData(), temp3);
+
+		node1->EraseNextNode();
+		ASSERT_TRUE(node1->GetNextNode() == nullptr);
+
+		ASSERT_EQ(node1->EraseNextNode(), false);
+
+		delete node1;
+	}
+
+public:
+	static void RunTest() {
+		InsertPreviousNodeTest();
+		InsertNextNodeTest();
+		//GetPreviousNodeTest();
+		//GetNextNodeTest();
+		ErasePreviousNodeTest();
+		EraseNextNodeTest();
+	}
+};
+
+void NodeTest() {
+	NodeConstructorTestSuite::RunTest();
+	NodeMethodTestSuite::RunTest();
+}
---
+++ typing_machine.cc
@@ -0,0 +1,95 @@
+// Copyright 2018 <Author>
+
+#include "typing_machine.h"
+
+const int MAX_STRING = 100;
+
+TypingMachine::TypingMachine() :
+_string_count(0)
+{
+	_cur = _head = _tail = new Node(' ');
+
+	return;
+}
+
+TypingMachine::~TypingMachine() {
+	Node *idx_ = _head;
+
+	while (idx_ != nullptr && idx_->EraseNextNode()) {
+		idx_->EraseNextNode();
+	}
+
+	delete _head;
+
+	return;
+}
+
+
+void TypingMachine::HomeKey() {
+	_cur = _head;
+	return;
+}
+
+void TypingMachine::EndKey() {
+	_cur = _tail;
+	return;
+}
+
+void TypingMachine::LeftKey() {
+	if (nullptr == _cur || nullptr == _cur->GetPreviousNode()) return;
+
+	_cur = _cur->GetPreviousNode();
+
+	return;
+}
+
+void TypingMachine::RightKey() {
+	if (nullptr == _cur || nullptr == _cur->GetNextNode()) return;
+
+	_cur = _cur->GetNextNode();
+
+	return;
+}
+
+bool TypingMachine::TypeKey(char key) {
+	if (key < 0x20 || key > 0x7E) return false;
+	if (_string_count == MAX_STRING) return false;
+
+	if (nullptr == _cur) {
+		_cur = _head = _tail = new Node(key);
+	}
+	else {
+		_cur->InsertPreviousNode(key);
+		if (_cur == _head) _head = _cur->GetPreviousNode();
+	}
+
+	_string_count++;
+	return true;
+}
+
+bool TypingMachine::EraseKey() {
+	if (nullptr == _cur->GetPreviousNode()) return false;
+
+	if (_cur->GetPreviousNode() == _head) _head = _cur;
+	_cur->ErasePreviousNode();
+
+	_string_count--;
+	return true;
+}
+
+std::string TypingMachine::Print(char separator) {
+	std::string str_("");
+
+	Node *idx_ = nullptr;
+
+	if (_head == _tail) {
+		if (0 != separator) str_.push_back(separator);
+		return str_;
+	}
+	for (idx_ = _head; idx_ != nullptr; idx_ = idx_->GetNextNode()) {
+		if (idx_ == _cur && 0 != separator) str_.push_back(separator);
+		if (idx_ != _tail) str_.push_back(idx_->GetData());
+	}
+
+	return str_;
+}
